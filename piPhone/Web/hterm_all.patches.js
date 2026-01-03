62fe9aa589954caa5a710530e6fc58a3c01cac7e
Gentris Leci <gentris.leci@localstack.cloud>
2026-01-03 01:52:41 +0100

    Update hterm_all* fiels to latest

--- piPhone/Web/hterm_all.patches.js
+++ piPhone/Web/hterm_all.patches.js
@@ -7,7 +7,7 @@ hterm.Terminal.prototype.onFocusChange__ = function(focused) {
   if (currentState === focused + '') {
     return;
   }
-  
+
   this.cursorNode_.setAttribute('focus', focused);
   this.restyleCursor_();
 
@@ -23,6 +23,8 @@ hterm.Terminal.prototype.overlaySize = function() {};
 
 hterm.Terminal.prototype.onMouse_ = function() {};
 
+// TODO: Remove our patch. htermjs supports cursorBlinkPause_ option now
+// see https://github.com/chromium/hterm/commit/f57d62de8f91f1fc8923fb000aeace041d063f9f
 hterm.Terminal.prototype.setCursorVisible = function(state) {
   this.options_.cursorVisible = state;
 
@@ -51,4 +53,16 @@ hterm.Terminal.prototype.setCursorVisible = function(state) {
       delete this.timeouts_.cursorBlink;
     }
   }
+};
+
+// NOTE(@nanzhong) hterm does not support DEC mode 1003 (any mouse event reporting mode).
+// DEC mode 1003 and DEC mode 1002 (which hterm does support) are almost identical. The only difference is that mode 1003 includes mouse movement tracking events which are rarely used.
+// This patches hterm to treat DEC mode 1003 the same as DEC mode 1002.
+
+hterm.VT.prototype.setDECMode_original = hterm.VT.prototype.setDECMode;
+hterm.VT.prototype.setDECMode = function(code, state) {
+  if (code === "1003") {
+    code = "1002";
+  }
+  hterm.VT.prototype.setDECMode_original.call(this, code, state);
 };
